ad36ef9caceab29f1bc82059758377fd9b4fe021
Your Name <feliperbmorais@gmail.com>
2022-09-14 23:49:47 -0300

    Alteração nos acruivos CSS, JAVA E HTML

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -1,4 +1,7 @@
 let botaoAvancar = document.querySelector("#botaoAvancar");
+let botaoAvancar2 = document.querySelector("#botaoAvancar2");
+let botaoVoltar = document.querySelector("#botaoVoltar");
+
 
 function avanco(){
  
